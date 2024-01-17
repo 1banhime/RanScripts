979c19c077c48f99a93e28cee333e0eac1f4826f
1banhime <125991946+1banhime@users.noreply.github.com>
2024-01-17 12:42:20 +0100

    Add files via upload

--- ldapimport.sh
+++ ldapimport.sh
@@ -15,16 +15,17 @@ fi
 #Used to use -e flag but it did not check if it was a regular file. it just checked if it exists
 if [ ! -f "$1" ]; then
     echo "FILE ERROR" >&2 #Exits script if incorrect file given as agrument 
-    exit 1 
+    exit 1
 fi
 
-while IFS="," read -r name uname passwd objclass; do #assigns the different fields in the CSV to variables
-	hashpass=sudo slappasswd $passwd 
+while IFS="," read -r name uname passwd objclass gid; do #assigns the different fields in the CSV to variables
+	hashpass=sudo slappasswd $passwd
 	#Creates a string for the ldapadd command to use
 	ldif=$(cat <<EOF
 		dn: uid=${username},${DN}
 		objectClass: ${objclass}
 		uid: ${username}
+		gid: ${gid}
 		cn: ${name}
 		userPassword: ${hashpass}
 		EOF
@@ -36,3 +37,5 @@ while IFS="," read -r name uname passwd objclass; do #assigns the different fiel
 			echo could not add $uname
 		fi 
 done < "$1" #Indicates the while loop to read from the arg
+
+#ai kotoba no 1banhime
